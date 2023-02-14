795bf0cbc4df0bf62996990035634acc4ea03f2c
Seonho Kim <sunhowold@naver.com>
2023-02-15 01:25:04 +0900

    initial commit

---
+++ src/main/java/podo/odeego/OdeegoApplication.java
@@ -0,0 +1,13 @@
+package podo.odeego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdeegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdeegoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/podo/odeego/OdeegoApplicationTests.java
@@ -0,0 +1,13 @@
+package podo.odeego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdeegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
