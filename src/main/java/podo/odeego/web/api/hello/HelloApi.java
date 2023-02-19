57c2cf9b835938cc4a2268e04691669d832bf2ae
hwankim123 <hwankim1941@gmail.com>
2023-02-19 17:45:11 +0900

    Feat(API): 테스트용 API 추가

---
+++ src/main/java/podo/odeego/web/api/hello/HelloApi.java
@@ -0,0 +1,23 @@
+package podo.odeego.web.api.hello;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloApi {
+
+    @GetMapping("/simple")
+    public String string() {
+        return "hello";
+    }
+
+    @GetMapping("/query-parameter")
+    public HelloResponse queryParam(@RequestParam String query) {
+        return new HelloResponse("hello " + query);
+    }
+
+    @GetMapping("/body")
+    public HelloResponse requestBody(@RequestBody HelloRequest request) {
+        return new HelloResponse(request.message());
+    }
+}
---
+++ src/main/java/podo/odeego/web/api/hello/HelloResponse.java
@@ -0,0 +1,6 @@
+package podo.odeego.web.api.hello;
+
+public record HelloResponse(
+        String message
+) {
+}
