3eff980a72b41d8e2bbb9dc93c36ed1312bf3af3
hwankim123 <hwankim1941@gmail.com>
2023-02-19 15:00:20 +0900

    Chore(CI): CI 세팅
    - PR을 올리면 자동으로 빌드 & 테스트

--- src/test/java/podo/odeego/OdeegoApplicationTests.java
+++ src/test/java/podo/odeego/OdeegoApplicationTests.java
@@ -1,13 +1,21 @@
 package podo.odeego;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OdeegoApplicationTests {
 
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void ciTest() {
+		log.info("CI test test");
+	}
 }
