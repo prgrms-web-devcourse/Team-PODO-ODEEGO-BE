adcaf6bc243c3cbe4095023ad8b093398b40fdaa
Seonho Kim <sunhowold@naver.com>
2023-02-22 00:11:25 +0900

    Refactor(DTO): 유지보수를 위해 DTO 클래스를 record 타입으로 migration

--- src/main/java/podo/odeego/infra/openapi/naver/dto/LocalSearchResponse.java
+++ src/main/java/podo/odeego/infra/openapi/naver/dto/LocalSearchResponse.java
@@ -2,44 +2,16 @@ package podo.odeego.infra.openapi.naver.dto;
 
 import java.util.List;
 
-public class LocalSearchResponse {
+import podo.odeego.domain.place.dto.SimplePlaceResponse;
+import podo.odeego.infra.openapi.naver.util.PlaceDataParser;
 
-	private String lastBuildDate;
-	private int total;
-	private int start;
-	private int display;
-	private List<LocalSearchItem> items;
-
-	public LocalSearchResponse() {
-	}
-
-	public LocalSearchResponse(String lastBuildDate, int total, int start, int display, List<LocalSearchItem> items) {
-		this.lastBuildDate = lastBuildDate;
-		this.total = total;
-		this.start = start;
-		this.display = display;
-		this.items = items;
-	}
-
-	public String getLastBuildDate() {
-		return lastBuildDate;
-	}
-
-	public int getTotal() {
-		return total;
-	}
-
-	public int getStart() {
-		return start;
-	}
-
-	public int getDisplay() {
-		return display;
-	}
-
-	public List<LocalSearchItem> getItems() {
-		return items;
-	}
+public record LocalSearchResponse(
+	String lastBuildDate,
+	int total,
+	int start,
+	int display,
+	List<LocalSearchItem> items
+) {
 
 	@Override
 	public String toString() {
@@ -53,69 +25,23 @@ public class LocalSearchResponse {
 			'}';
 	}
 
-	private static class LocalSearchItem {
+	public List<SimplePlaceResponse> getPlaces() {
+		return this.items.stream()
+			.map(item -> new SimplePlaceResponse(PlaceDataParser.trimHtmlTags(item.title), item.roadAddress))
+			.toList();
+	}
 
-		private String title;
-		private String link;
-		private String category;
-		private String description;
-		private String telephone;
-		private String address;
-		private String roadAddress;
-		private int mapx;
-		private int mapy;
-
-		public LocalSearchItem() {
-		}
-
-		public LocalSearchItem(String title, String link, String category, String description, String telephone,
-			String address, String roadAddress, int mapx, int mapy) {
-			this.title = title;
-			this.link = link;
-			this.category = category;
-			this.description = description;
-			this.telephone = telephone;
-			this.address = address;
-			this.roadAddress = roadAddress;
-			this.mapx = mapx;
-			this.mapy = mapy;
-		}
-
-		public String getTitle() {
-			return title;
-		}
-
-		public String getLink() {
-			return link;
-		}
-
-		public String getCategory() {
-			return category;
-		}
-
-		public String getDescription() {
-			return description;
-		}
-
-		public String getTelephone() {
-			return telephone;
-		}
-
-		public String getAddress() {
-			return address;
-		}
-
-		public String getRoadAddress() {
-			return roadAddress;
-		}
-
-		public int getMapx() {
-			return mapx;
-		}
-
-		public int getMapy() {
-			return mapy;
-		}
+	private record LocalSearchItem(
+		String title,
+		String link,
+		String category,
+		String description,
+		String telephone,
+		String address,
+		String roadAddress,
+		int mapx,
+		int mapy
+	) {
 
 		@Override
 		public String toString() {
