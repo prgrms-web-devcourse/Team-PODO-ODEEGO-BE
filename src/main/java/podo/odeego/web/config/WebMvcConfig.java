6e5c33e24c8ef3a594b10fcca85607ca0e1dbda1
hwankim123 <hwankim1941@gmail.com>
2023-02-21 17:00:29 +0900

    Feat(Web): CORS 설정 추가
    - api 서버의 모든 요청에 대해 허용
    - aws 배포 도메인의 출처를 허용
    - 모든 Http Method에 대해 허용

---
+++ src/main/java/podo/odeego/web/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package podo.odeego.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://52.78.224.123:8080")
+			.allowedMethods("*");
+	}
+}
