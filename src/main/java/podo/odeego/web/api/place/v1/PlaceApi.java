8eb94251401e65ec3d7134fab4d2ad88dc10262b
Seonho Kim <sunhowold@naver.com>
2023-02-22 00:15:56 +0900

    Feat(API): 지하철역 근처의 주변 장소를 추천하는 컨트롤러 구현

---
+++ src/main/java/podo/odeego/web/api/place/v1/PlaceApi.java
@@ -0,0 +1,38 @@
+package podo.odeego.web.api.place.v1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import podo.odeego.domain.place.domain.PlaceCategory;
+import podo.odeego.domain.place.dto.PlaceResponses;
+import podo.odeego.domain.place.service.PlaceQueryService;
+import podo.odeego.domain.subway.dto.StationInfo;
+
+@RestController
+@RequestMapping("/api/v1/places")
+public class PlaceApi {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	private final PlaceQueryService placeQueryService;
+
+	public PlaceApi(@Qualifier("naverPlaceQueryService") PlaceQueryService placeQueryService) {
+		this.placeQueryService = placeQueryService;
+	}
+
+	@GetMapping
+	public ResponseEntity<PlaceResponses> getAll(
+		@RequestParam(name = "station-name") String stationName,
+		@RequestParam String address,
+		@RequestParam(required = false) PlaceCategory category
+	) {
+		PlaceResponses response = placeQueryService.getAll(new StationInfo(stationName, address), category);
+		return ResponseEntity.ok(response);
+	}
+}
