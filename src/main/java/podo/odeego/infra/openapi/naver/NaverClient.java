819075a6c03ecbb8ad81b157c54ddb1e65213201
Seonho Kim <sunhowold@naver.com>
2023-02-21 23:59:18 +0900

    Feat(infra): 질의어와 장소 카테고리만으로 open api를 호출하는 기능 추가

--- src/main/java/podo/odeego/infra/openapi/naver/NaverClient.java
+++ src/main/java/podo/odeego/infra/openapi/naver/NaverClient.java
@@ -1,6 +1,8 @@
 package podo.odeego.infra.openapi.naver;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import podo.odeego.domain.place.domain.PlaceCategory;
+import podo.odeego.domain.place.dto.SimplePlaceResponse;
 import podo.odeego.infra.openapi.naver.dto.LocalSearchRequest;
 import podo.odeego.infra.openapi.naver.dto.LocalSearchResponse;
 
@@ -34,7 +38,31 @@ public class NaverClient {
 	@Value("${naver.url.search.local}")
 	private String localSearchUrl;
 
-	public LocalSearchResponse searchLocal(LocalSearchRequest request) {
+	public List<SimplePlaceResponse> searchLocal(String query) {
+		return Arrays.stream(PlaceCategory.values())
+			.map(category -> searchLocal(query, category))
+			.flatMap(List::stream)
+			.distinct()
+			.toList();
+	}
+
+	public List<SimplePlaceResponse> searchLocal(String query, PlaceCategory category) {
+		return getLocalSearchRequests(query, category).stream()
+			.map(this::callLocalSearchApi)
+			.map(LocalSearchResponse::getPlaces)
+			.flatMap(List::stream)
+			.distinct()
+			.toList();
+	}
+
+	private List<LocalSearchRequest> getLocalSearchRequests(String query, PlaceCategory category) {
+		return List.of(
+			LocalSearchRequest.of(query, category, LocalSearchRequest.SortType.RANDOM),
+			LocalSearchRequest.of(query, category, LocalSearchRequest.SortType.COMMENT)
+		);
+	}
+
+	private LocalSearchResponse callLocalSearchApi(LocalSearchRequest request) {
 		URI uri = getUri(request);
 		HttpEntity<HttpHeaders> httpEntity = getHttpEntity();
 		ParameterizedTypeReference<LocalSearchResponse> responseType = new ParameterizedTypeReference<>() {
--- src/test/java/podo/odeego/infra/openapi/naver/NaverClientTest.java
+++ src/test/java/podo/odeego/infra/openapi/naver/NaverClientTest.java
@@ -3,6 +3,8 @@ package podo.odeego.infra.openapi.naver;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -10,8 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import podo.odeego.infra.openapi.naver.dto.LocalSearchRequest;
-import podo.odeego.infra.openapi.naver.dto.LocalSearchResponse;
+import podo.odeego.domain.place.domain.PlaceCategory;
+import podo.odeego.domain.place.dto.SimplePlaceResponse;
 
 @SpringBootTest
 class NaverClientTest {
@@ -25,22 +27,34 @@ class NaverClientTest {
 	@DisplayName("네이버 지역 검색 API를 호출할 수 있다.")
 	void call_naver_local_search_api() {
 		// given
-		LocalSearchRequest cafeRequest =
-			LocalSearchRequest.newCafeRequest("강남역", LocalSearchRequest.SortType.RANDOM);
-		LocalSearchRequest restaurantRequest =
-			LocalSearchRequest.newRestaurantRequest("강남역", LocalSearchRequest.SortType.RANDOM);
+		String query = "강남역";
 
 		// when
-		LocalSearchResponse cafeResponse = naverClient.searchLocal(cafeRequest);
-		LocalSearchResponse restaurantResponse = naverClient.searchLocal(restaurantRequest);
+		List<SimplePlaceResponse> responses = naverClient.searchLocal(query);
+
+		// then
+		assertThat(responses).isNotNull();
+		for (SimplePlaceResponse response : responses) {
+			log.info(response.toString());
+		}
+	}
+
+	@Test
+	@DisplayName("장소 카테고리 별로 네이버 지역 검색 API를 호출할 수 있다.")
+	void call_naver_local_search_api_by_place_category() {
+		// given
+		String query = "강남역";
+		PlaceCategory cafe = PlaceCategory.CAFE;
+		PlaceCategory restaurant = PlaceCategory.RESTAURANT;
+
+		// when
+		List<SimplePlaceResponse> searchCafeResponses = naverClient.searchLocal(query, cafe);
+		List<SimplePlaceResponse> searchRestaurantResponses = naverClient.searchLocal(query, restaurant);
 
 		// then
 		assertAll(
-			() -> assertThat(cafeResponse).isNotNull(),
-			() -> assertThat(restaurantResponse).isNotNull()
+			() -> assertThat(searchCafeResponses).isNotNull(),
+			() -> assertThat(searchRestaurantResponses).isNotNull()
 		);
-
-		log.info(cafeResponse.toString());
-		log.info(restaurantResponse.toString());
 	}
 }
