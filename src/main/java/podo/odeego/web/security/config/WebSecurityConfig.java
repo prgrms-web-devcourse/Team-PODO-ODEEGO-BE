9672dd4f0ab801db4bf94ed566be3fca9fb811e4
hwankim123 <hwankim1941@gmail.com>
2023-02-21 15:53:35 +0900

    Feat: OAuth2로 최초 로그인한 사용자를 회원가입 시키도록 구현
    - OAuth2AuthenticationSuccessHandler 에 사용자를 회원가입시키도록 구현

--- src/main/java/podo/odeego/web/security/config/WebSecurityConfig.java
+++ src/main/java/podo/odeego/web/security/config/WebSecurityConfig.java
@@ -4,22 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import podo.odeego.domain.user.service.UserService;
 import podo.odeego.web.security.oauth2.OAuth2AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .oauth2Login()
-                .successHandler(oAuth2AuthenticationSuccessHandler());
+	private final UserService userService;
 
-        return http.build();
-    }
+	public WebSecurityConfig(UserService userService) {
+		this.userService = userService;
+	}
 
-    @Bean
-    public OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler(){
-        return new OAuth2AuthenticationSuccessHandler();
-    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.oauth2Login()
+			.successHandler(oAuth2AuthenticationSuccessHandler());
+
+		return http.build();
+	}
+
+	@Bean
+	public OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler() {
+		return new OAuth2AuthenticationSuccessHandler(userService);
+	}
 }
--- src/main/java/podo/odeego/web/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/podo/odeego/web/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -1,26 +1,41 @@
 package podo.odeego.web.security.oauth2;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import podo.odeego.domain.user.service.UserService;
 
 public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @Override
-    public void onAuthenticationSuccess(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Authentication authentication
-    ) throws ServletException, IOException {
-        Object principal = authentication.getPrincipal();
-        log.info("OAuth2 Authentication Success: {}", principal);
-    }
+	private final UserService userService;
+
+	public OAuth2AuthenticationSuccessHandler(UserService userService) {
+		this.userService = userService;
+	}
+
+	@Override
+	public void onAuthenticationSuccess(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		Authentication authentication
+	) throws ServletException, IOException {
+		if (authentication instanceof OAuth2AuthenticationToken) {
+			OAuth2AuthenticationToken token = (OAuth2AuthenticationToken)authentication;
+			OAuth2User oauth2User = token.getPrincipal();
+			String provider = token.getAuthorizedClientRegistrationId();
+			// userService.join(oauth2User, )
+		}
+	}
 }
