9011a91920c94a0ebe0dcf9be18ed379b6733fee
hwankim123 <hwankim1941@gmail.com>
2023-02-22 13:33:51 +0900

    Refactor: 허용할 origin을 yml 설정파일을 통해서 받도록 수정

--- src/main/java/podo/odeego/web/config/WebMvcConfig.java
+++ src/main/java/podo/odeego/web/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package podo.odeego.web.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+	private final String origin;
+
+	public WebMvcConfig(@Value("${server.host.front}") String origin) {
+		this.origin = origin;
+	}
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://52.78.224.123:8080")
+			.allowedOrigins(origin)
 			.allowedMethods("*");
 	}
 }
