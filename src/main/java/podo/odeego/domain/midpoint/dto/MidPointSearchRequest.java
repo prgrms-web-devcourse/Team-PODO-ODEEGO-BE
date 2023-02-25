e5e4d0594cf7537aa68d54b71fb6f98e4b2b50eb
dlxortmd987 <dlxortmd987@gmail.com>
2023-02-25 21:31:03 +0900

    Feat(Dto): 중간 지점 찾기 요청 객체 구현

---
+++ src/main/java/podo/odeego/domain/midpoint/dto/MidPointSearchRequest.java
@@ -0,0 +1,50 @@
+package podo.odeego.domain.midpoint.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
+
+public record MidPointSearchRequest(
+
+	@NotNull
+	@Size(min = 1, max = 4, message = "출발지의 개수를 확인해주세요.")
+	List<Start> stations
+) {
+	public record Start(
+
+		@NotBlank
+		String name,
+
+		@NotNull
+		@Range
+		double lat,
+
+		@NotNull
+		@DecimalMin("0.0")
+		double lng
+	) {
+	}
+
+	public boolean isAllSameStart() {
+		return stations.stream()
+			.collect(Collectors.groupingBy(Start::name))
+			.size() == 1;
+	}
+
+	public List<String> getStartNames() {
+		return stations.stream()
+			.map(Start::name)
+			.toList();
+	}
+
+	public String getFirstStart() {
+		return stations.get(0)
+			.name();
+	}
+}
