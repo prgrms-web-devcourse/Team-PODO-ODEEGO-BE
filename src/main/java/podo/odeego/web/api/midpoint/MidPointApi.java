f149b304cd8d9da195abd9b8f793c44a69a91260
dlxortmd987 <dlxortmd987@gmail.com>
2023-02-25 21:30:31 +0900

    Feat(Api): 중간 지점 찾기 구현

---
+++ src/main/java/podo/odeego/web/api/midpoint/MidPointApi.java
@@ -0,0 +1,30 @@
+package podo.odeego.web.api.midpoint;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import podo.odeego.domain.midpoint.dto.MidPointSearchRequest;
+import podo.odeego.domain.midpoint.dto.MidPointSearchResponse;
+import podo.odeego.domain.midpoint.service.MidPointService;
+
+@RestController
+@RequestMapping("/api/v1/midpoint")
+public class MidPointApi {
+
+	private final MidPointService midPointService;
+
+	public MidPointApi(MidPointService midPointService) {
+		this.midPointService = midPointService;
+	}
+
+	@PostMapping("/search")
+	public ResponseEntity<MidPointSearchResponse> search(
+		@RequestBody @Valid MidPointSearchRequest midPointSearchRequest) {
+		return ResponseEntity.ok(midPointService.search(midPointSearchRequest));
+	}
+}
