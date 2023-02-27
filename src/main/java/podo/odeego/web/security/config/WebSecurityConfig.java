c55c78223fe7698527f163fd7a285690ddd09120
Seonho Kim <sunhowold@naver.com>
2023-02-27 12:36:33 +0900

    Refactor(security): CSRF 설정 추가

--- src/main/java/podo/odeego/web/security/config/WebSecurityConfig.java
+++ src/main/java/podo/odeego/web/security/config/WebSecurityConfig.java
@@ -20,6 +20,7 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable()
 			.formLogin().disable();
 
 		http
