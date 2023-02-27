467be56e753a11b93593ac16919265aa58612681
Seonho Kim <sunhowold@naver.com>
2023-02-27 20:45:10 +0900

    Feat(entity): 엔티티 생성시각, 수정시각을 표현하는 추상 객체 구현

---
+++ src/main/java/podo/odeego/domain/type/BaseTime.java
@@ -0,0 +1,31 @@
+package podo.odeego.domain.type;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime modifiedAt;
+
+	public LocalDateTime createdAt() {
+		return createdAt;
+	}
+
+	public LocalDateTime modifiedAt() {
+		return modifiedAt;
+	}
+}
