8f9fe8d9eaa6d61784b07622291109c0d08f1e19
Seonho Kim <sunhowold@naver.com>
2023-02-27 20:56:21 +0900

    Feat(entity): 모임 엔티티 객체 구현

--- src/main/java/podo/odeego/domain/member/entity/Member.java
+++ src/main/java/podo/odeego/domain/member/entity/Member.java
@@ -1,11 +1,17 @@
 package podo.odeego.domain.member.entity;
 
+import static javax.persistence.FetchType.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import podo.odeego.domain.group.entity.Group;
+import podo.odeego.domain.group.exception.AlreadyParticipatingGroupException;
 import podo.odeego.domain.type.BaseTime;
 
 @Entity
@@ -25,6 +31,10 @@ public class Member extends BaseTime {
 	@Column(nullable = false, length = 80)
 	private String providerId;
 
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "group_id")
+	private Group group;
+
 	protected Member() {
 	}
 
@@ -34,8 +44,28 @@ public class Member extends BaseTime {
 		this.providerId = providerId;
 	}
 
+	public void participateGroup(Group group) {
+		this.group = group;
+	}
+
+	public void verifyNonOfGroupParticipating() {
+		if (isParticipatingGroup()) {
+			throw new AlreadyParticipatingGroupException(
+				"Member '%d' is already participating group '%s'.".formatted(id(), this.group().id().toString())
+			);
+		}
+	}
+
+	private boolean isParticipatingGroup() {
+		return this.group != null;
+	}
+
 	public Long id() {
 		return id;
+	}
+
+	public Group group() {
+		return group;
 	}
 
 	public String username() {
