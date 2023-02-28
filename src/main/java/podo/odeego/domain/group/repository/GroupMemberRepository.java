fde8dd42aebe26e3cc433df4b5546b1e36ccb07e
dlxortmd987 <dlxortmd987@gmail.com>
2023-02-28 19:23:13 +0900

    Feat(repository): 그룹에 따른 그룹 구성원 찾는 메서드 작성

--- src/main/java/podo/odeego/domain/group/repository/GroupMemberRepository.java
+++ src/main/java/podo/odeego/domain/group/repository/GroupMemberRepository.java
@@ -1,8 +1,13 @@
 package podo.odeego.domain.group.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import podo.odeego.domain.group.entity.Group;
 import podo.odeego.domain.group.entity.GroupMember;
 
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
+
+	List<GroupMember> findAllByGroup(Group group);
 }
