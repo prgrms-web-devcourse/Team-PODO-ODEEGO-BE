ef177b29e9d04b3cc121f4e6c661b5593f773967
Seonho Kim <sunhowold@naver.com>
2023-02-28 18:14:28 +0900

    Feat(service): 그룹 단건 조회 기능 구현

--- src/main/java/podo/odeego/domain/group/service/GroupQueryService.java
+++ src/main/java/podo/odeego/domain/group/service/GroupQueryService.java
@@ -1,12 +1,17 @@
 package podo.odeego.domain.group.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import podo.odeego.domain.group.dto.Participants;
+import podo.odeego.domain.group.dto.response.GroupResponse;
 import podo.odeego.domain.group.entity.Group;
+import podo.odeego.domain.group.entity.GroupMember;
 import podo.odeego.domain.group.exception.GroupNotFoundException;
+import podo.odeego.domain.group.repository.GroupMemberRepository;
 import podo.odeego.domain.group.repository.GroupRepository;
 
 @Service
@@ -14,13 +19,29 @@ import podo.odeego.domain.group.repository.GroupRepository;
 public class GroupQueryService {
 
 	private final GroupRepository groupRepository;
+	private final GroupMemberRepository groupMemberRepository;
 
-	public GroupQueryService(GroupRepository groupRepository) {
+	public GroupQueryService(GroupRepository groupRepository, GroupMemberRepository groupMemberRepository) {
 		this.groupRepository = groupRepository;
+		this.groupMemberRepository = groupMemberRepository;
+	}
+
+	public GroupResponse getOne(UUID groupId) {
+		Group findGroup = findById(groupId);
+		List<GroupMember> findGroupMembers = groupMemberRepository.findGroupMembersByGroup(findGroup);
+		return GroupResponse.from(findGroup, Participants.from(findGroupMembers));
 	}
 
 	public Group findById(UUID groupId) {
 		return groupRepository.findById(groupId)
-			.orElseThrow(() -> new GroupNotFoundException(""));
+			.orElseThrow(() -> new GroupNotFoundException(
+				"Cannot find group for groupId=%s".formatted(groupId.toString()))
+			);
+	}
+
+	public void verifyGroupExists(UUID groupId) {
+		if (!groupRepository.existsById(groupId)) {
+			throw new GroupNotFoundException("Cannot find group for groupId=%s".formatted(groupId.toString()));
+		}
 	}
 }
