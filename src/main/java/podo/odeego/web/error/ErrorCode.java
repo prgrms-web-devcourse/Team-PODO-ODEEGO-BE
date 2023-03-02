7cc8bfc2b6be999e1ef56ab10ef9266c62aedcd8
Seonho Kim <sunhowold@naver.com>
2023-03-02 23:19:51 +0900

    Chore(exception): 가독성 향상을 위한 도메인 별 에러코드 개행

--- src/main/java/podo/odeego/web/error/ErrorCode.java
+++ src/main/java/podo/odeego/web/error/ErrorCode.java
@@ -7,11 +7,16 @@ public enum ErrorCode {
 	// Common
 	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Invalid Input Value"),
 	INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, "Invalid Type Value"),
-	NOT_FOUND(HttpStatus.NOT_FOUND, "Not Found Error"),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Server Error"),
-	GROUP_HOST_ABSENT(HttpStatus.INTERNAL_SERVER_ERROR, "Group Host Absent"),
 	FORBIDDEN(HttpStatus.FORBIDDEN, "Forbidden"),
-	NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "Not Acceptable");
+	NOT_FOUND(HttpStatus.NOT_FOUND, "Not Found Error"),
+	NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "Not Acceptable"),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Server Error"),
+
+	// Member
+	MEMBER_DUPLICATED(HttpStatus.BAD_REQUEST, "Member Duplicated"),
+
+	// Group
+	GROUP_HOST_ABSENT(HttpStatus.INTERNAL_SERVER_ERROR, "Group Host Absent");
 
 	private final HttpStatus status;
 	private final String message;
