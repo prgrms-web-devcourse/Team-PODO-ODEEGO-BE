a958b67098ba612af0dd9ac048481063207f4b72
hwankim123 <hwankim1941@gmail.com>
2023-03-03 17:59:54 +0900

    Refactor(Security): merge로 인해 생긴 이전 코드 삭제

--- src/main/java/podo/odeego/web/security/config/WebSecurityConfig.java
+++ src/main/java/podo/odeego/web/security/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -33,12 +32,6 @@ public class WebSecurityConfig {
 		this.localOrigin = localOrigin;
 		this.releaseOrigin = releaseOrigin;
 		this.jwtProvider = jwtProvider;
-	}
-
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> web.ignoring()
-			.antMatchers("/api/v1/auth/**");
 	}
 
 	@Bean
