8aba425c76ffb0108d0362e10ed9a4a279e7ec2c
hwankim123 <hwankim1941@gmail.com>
2023-03-07 16:31:28 +0900

    Feature(Auth): 인증 로직을 수행하는 객체 추가.

---
+++ src/main/java/podo/odeego/web/security/service/AuthorizationComponent.java
@@ -0,0 +1,53 @@
+package podo.odeego.web.security.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import podo.odeego.domain.member.dto.MemberJoinResponse;
+import podo.odeego.domain.member.service.MemberService;
+import podo.odeego.infra.openapi.kakao.KakaoClient;
+import podo.odeego.infra.openapi.kakao.dto.KakaoProfileResponse;
+import podo.odeego.web.api.auth.dto.MemberLoginResponse;
+import podo.odeego.web.security.jwt.JwtProvider;
+
+@Component
+public class AuthorizationComponent {
+
+	private final KakaoClient kakaoClient;
+	private final MemberService memberService;
+	private final JwtProvider jwtProvider;
+	private final String provider;
+
+	public AuthorizationComponent(
+		KakaoClient kakaoClient,
+		MemberService memberService,
+		JwtProvider jwtProvider,
+		@Value("${oauth2.client.provider.name}") String provider
+	) {
+		this.kakaoClient = kakaoClient;
+		this.memberService = memberService;
+		this.jwtProvider = jwtProvider;
+		this.provider = provider;
+	}
+
+	public MemberLoginResponse getMemberInfo(String oAuth2Token) {
+		KakaoProfileResponse kakaoProfile = kakaoClient.getUserInfo(oAuth2Token);
+		MemberJoinResponse joinResponse = memberService.join(
+			kakaoProfile.profileImageUrl(),
+			provider,
+			kakaoProfile.providerId().toString());
+
+		return responseLoginSuccess(joinResponse);
+	}
+
+	private MemberLoginResponse responseLoginSuccess(MemberJoinResponse memberJoinResponse) {
+		String accessToken = jwtProvider.generateAccessToken(memberJoinResponse.id());
+		String refreshToken = jwtProvider.generateRefreshToken(memberJoinResponse.id());
+		return new MemberLoginResponse(
+			accessToken,
+			refreshToken,
+			memberJoinResponse.memberType(),
+			memberJoinResponse.profileImageUrl()
+		);
+	}
+}
