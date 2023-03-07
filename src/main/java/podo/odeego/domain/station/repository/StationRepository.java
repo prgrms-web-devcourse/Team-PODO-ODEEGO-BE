765ed18440845b5a2be3dba59f70f5a723b9c62c
Seonho Kim <sunhowold@naver.com>
2023-03-07 21:13:55 +0900

    Fix(repository): JPA에서 DTO로 바로 조회시 적절한 생성자를 찾지 못해 발생한 역직렬화 오류 해결

--- src/main/java/podo/odeego/domain/station/repository/StationRepository.java
+++ src/main/java/podo/odeego/domain/station/repository/StationRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import podo.odeego.domain.station.dto.StationNameQueryDto;
 import podo.odeego.domain.station.entity.Station;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
@@ -14,8 +15,8 @@ public interface StationRepository extends JpaRepository<Station, Long> {
 	boolean existsByName(String name);
 
 	@Query("""
-		select s
+		select new podo.odeego.domain.station.dto.StationNameQueryDto(s.name)
 		from Station s
 		group by s.name""")
-	List<Station> findAllGroupByName();
+	List<StationNameQueryDto> findAllGroupByName();
 }
