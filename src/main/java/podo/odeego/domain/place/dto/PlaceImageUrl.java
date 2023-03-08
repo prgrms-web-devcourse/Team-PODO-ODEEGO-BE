1c2ff4277a32efe20a31b8eca6c1f7072c06ed4e
Seonho Kim <sunhowold@naver.com>
2023-03-08 14:51:30 +0900

    Refactor(DTO): 불변성을 명시하기 위해 DTO 클래스 record 타입으로 변경

--- src/main/java/podo/odeego/domain/place/dto/PlaceImageUrl.java
+++ src/main/java/podo/odeego/domain/place/dto/PlaceImageUrl.java
@@ -4,21 +4,13 @@ import java.util.List;
 
 import podo.odeego.domain.place.entity.PlaceImage;
 
-public class PlaceImageUrl {
-
-	private String url;
-
-	private PlaceImageUrl(String url) {
-		this.url = url;
-	}
+public record PlaceImageUrl(
+	String url
+) {
 
 	public static List<PlaceImageUrl> from(List<PlaceImage> images) {
 		return images.stream()
 			.map(image -> new PlaceImageUrl(image.getUrl()))
 			.toList();
-	}
-
-	public String getUrl() {
-		return url;
 	}
 }
