1555673ea81496191195a82d43ee567ab688eaaa
Seonho Kim <sunhowold@naver.com>
2023-03-09 22:52:05 +0900

    Test: 미사용 테스트 제거

--- src/test/java/podo/odeego/OdeegoApplicationTests.java
+++ src/test/java/podo/odeego/OdeegoApplicationTests.java
@@ -1,26 +1,12 @@
 package podo.odeego;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OdeegoApplicationTests {
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
-
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void ciTest() {
-		log.info("CI test test");
-	}
-
-	@Test
-	void cdTest() {
-		log.info("CD test test");
 	}
 }
