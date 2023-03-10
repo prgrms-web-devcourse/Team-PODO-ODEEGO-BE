ef1e54d6ec341910b2c745e8c234732c802f5be0
hwankim123 <hwankim1941@gmail.com>
2023-03-10 16:13:17 +0900

    Hotfix: EanbleWebMvc 어노테이션으로 인해 시간 데이터 Json formate이 배열로 직렬화되는 문제 해결

--- src/main/java/podo/odeego/web/config/WebMvcConfig.java
+++ src/main/java/podo/odeego/web/config/WebMvcConfig.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import podo.odeego.web.api.place.converter.PlaceCategoryConverter;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	private final String localOrigin;
