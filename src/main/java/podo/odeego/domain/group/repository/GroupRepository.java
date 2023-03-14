66be63ed8a56ef238b51d5d17adcf8945d48dbd1
dlxortmd987 <dlxortmd987@gmail.com>
2023-03-14 16:40:04 +0900

    Feat(repository): 그룹 멤버가 이미 출발지를 제출했는지 확인하는 API 구현

--- src/main/java/podo/odeego/domain/group/repository/GroupRepository.java
+++ src/main/java/podo/odeego/domain/group/repository/GroupRepository.java
@@ -1,12 +1,21 @@
 package podo.odeego.domain.group.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import podo.odeego.domain.group.entity.Group;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, UUID> {
+
+	@Query("""
+		select distinct g
+		from Group g
+		join fetch 	g.groupMembers
+		""")
+	Optional<Group> findFetchById(UUID groupId);
 }
