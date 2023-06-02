a163a763a73a18b8230e609ac3cc3fb352550e1f
hwankim123 <hwankim1941@gmail.com>
2023-06-02 18:03:35 +0900

    Test: 리팩토링 사항에 대한 단위 테스트, 통합 테스트(OAuth2는 모킹) 작성

---
+++ src/test/java/podo/odeego/web/auth/MemberLoginManagerTest.java
@@ -0,0 +1,56 @@
+package podo.odeego.web.auth;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import podo.odeego.domain.auth.service.RefreshTokenService;
+import podo.odeego.domain.member.dto.MemberJoinResponse;
+import podo.odeego.domain.member.entity.MemberType;
+import podo.odeego.web.api.auth.dto.MemberLoginResponse;
+
+@ExtendWith(MockitoExtension.class)
+class MemberLoginManagerTest {
+
+	@InjectMocks
+	MemberLoginManager memberLoginManager;
+
+	@Mock
+	MemberJoinManager memberJoinManager;
+
+	@Mock
+	RefreshTokenService refreshTokenService;
+
+	@Mock
+	JwtProvider jwtProvider;
+
+	@DisplayName("MemberLoginResponse의 login 기능을 통해 OAuth2 기반의 로그인이 진행되고, accessToken과 RefreshToken을 반환받습니다.")
+	@Test
+	public void login() {
+		//given
+		MemberJoinResponse joinResponse = new MemberJoinResponse(1L, "profileImageUrl", MemberType.PRE);
+		doReturn(joinResponse).when(memberJoinManager)
+			.join("oAuth2Token");
+
+		String accessToken = "accessToken";
+		doReturn(accessToken).when(jwtProvider)
+			.generateAccessToken(joinResponse.id());
+
+		String refreshToken = "refreshToken";
+		doReturn(refreshToken).when(refreshTokenService)
+			.save(joinResponse.id());
+
+		//when
+		MemberLoginResponse loginResponse = memberLoginManager.login("oAuth2Token");
+
+		//then
+		assertThat(loginResponse.getAccessToken()).isEqualTo("accessToken");
+		assertThat(loginResponse.getRefreshToken()).isEqualTo("refreshToken");
+	}
+}
