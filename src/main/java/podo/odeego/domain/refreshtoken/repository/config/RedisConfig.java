f8eaf67a29725aa9a64a5540016f2659fa1f25e2
hwankim123 <hwankim1941@gmail.com>
2023-06-09 15:39:45 +0900

    Refactor: redis host 및 port 설정 추기

--- src/main/java/podo/odeego/domain/refreshtoken/repository/config/RedisConfig.java
+++ src/main/java/podo/odeego/domain/refreshtoken/repository/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package podo.odeego.domain.refreshtoken.repository.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,9 +10,20 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfig {
 
+	private final String host;
+	private final int port;
+
+	public RedisConfig(
+		@Value("${spring.redis.host}") String host,
+		@Value("${spring.redis.port}") int port
+	) {
+		this.host = host;
+		this.port = port;
+	}
+
 	@Bean
 	public LettuceConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory();
+		return new LettuceConnectionFactory(host, port);
 	}
 
 	@Bean
