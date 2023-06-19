5c553d65d9c55d61f5b4e16680e14ff66654b000
hwankim123 <hwankim1941@gmail.com>
2023-06-19 14:16:15 +0900

    Refactor: 406 에러 해결

--- src/main/java/podo/odeego/web/auth/dto/LoginResponse.java
+++ src/main/java/podo/odeego/web/auth/dto/LoginResponse.java
@@ -4,10 +4,13 @@ import podo.odeego.domain.member.entity.MemberType;
 
 public class LoginResponse {
 
-	private final String accessToken;
-	private final String refreshToken;
-	private final MemberType memberType;
-	private final String profileImageUrl;
+	private String accessToken;
+	private String refreshToken;
+	private MemberType memberType;
+	private String profileImageUrl;
+
+	private LoginResponse() {
+	}
 
 	private LoginResponse(String accessToken, String refreshToken, MemberType memberType,
 		String profileImageUrl) {
@@ -24,5 +27,21 @@ public class LoginResponse {
 			joinResponse.memberType(),
 			joinResponse.profileImageUrl()
 		);
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public MemberType getMemberType() {
+		return memberType;
+	}
+
+	public String getProfileImageUrl() {
+		return profileImageUrl;
 	}
 }
