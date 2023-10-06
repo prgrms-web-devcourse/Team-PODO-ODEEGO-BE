267975e9a4e7f853aba6dd7711b778f91a7b865c
hwankim123 <hwankim1941@gmail.com>
2023-10-06 15:38:08 +0900

    Refactor: RefreshTokenRepositoryTest 패키지 명에 맞게 수정

---
+++ src/test/java/podo/odeego/domain/refreshtoken/repository/RefreshTokenRepositoryTest.java
@@ -0,0 +1,36 @@
+package podo.odeego.domain.refreshtoken.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import podo.odeego.config.TestRedisConfig;
+import podo.odeego.domain.refreshtoken.entity.RefreshToken;
+
+@SpringBootTest(classes = TestRedisConfig.class)
+class RefreshTokenRepositoryTest {
+
+	@Autowired
+	private RefreshTokenRepository refreshTokenRepository;
+
+	@Test
+	@DisplayName("RefreshToken을 저장할 수 있습니다.")
+	public void saveRefreshToken() {
+		//given
+		String actualUUID = UUID.randomUUID().toString();
+
+		//when
+		RefreshToken refreshToken = RefreshToken.of(actualUUID, 1L);
+		refreshTokenRepository.save(refreshToken);
+		String expectedUUID = refreshTokenRepository.findById(refreshToken.token())
+			.get().token();
+
+		//then
+		assertThat(expectedUUID).isEqualTo(actualUUID);
+	}
+}
