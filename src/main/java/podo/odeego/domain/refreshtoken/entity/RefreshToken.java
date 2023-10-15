ae67c5e9783cfa65a9834befe03c74d06b806569
hwankim123 <hwankim1941@gmail.com>
2023-10-15 14:57:23 +0900

    Feature: RefreshToken 클래스 구현
    - Redis에 직렬화되어서 저장될 Key 클래스

--- src/main/java/podo/odeego/domain/refreshtoken/entity/RefreshToken.java
+++ src/main/java/podo/odeego/domain/refreshtoken/entity/RefreshToken.java
@@ -1,41 +1,18 @@
 package podo.odeego.domain.refreshtoken.entity;
 
-import java.util.UUID;
+import java.io.Serializable;
 
-import org.springframework.data.annotation.Id;
+public class RefreshToken implements Serializable {
 
-public class RefreshToken {
-
-	@Id
 	private final String token;
-	private final Long memberId;
+	private final boolean isValid;
 
-	private RefreshToken(String token, Long memberId) {
+	public RefreshToken(String token, boolean isValid) {
 		this.token = token;
-		this.memberId = memberId;
+		this.isValid = isValid;
 	}
 
-	public static RefreshToken randomUUID(Long memberId) {
-		return new RefreshToken(UUID.randomUUID().toString(), memberId);
-	}
-
-	public static RefreshToken of(String refreshToken, Long memberId) {
-		return new RefreshToken(refreshToken, memberId);
-	}
-
-	public String token() {
+	public String getToken() {
 		return token;
-	}
-
-	public Long memberId() {
-		return memberId;
-	}
-
-	@Override
-	public String toString() {
-		return "RefreshToken{" +
-			"token='" + token + '\'' +
-			", memberId=" + memberId +
-			'}';
 	}
 }
