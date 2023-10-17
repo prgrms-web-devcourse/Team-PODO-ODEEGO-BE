80c6cf75b1843388c6e76fb3b34c17327d8e8db8
hwankim123 <hwankim1941@gmail.com>
2023-10-17 10:44:28 +0900

    Refactor: Redis key-value 구조 변경
    - 기존: RefreshToken(POJO):memberId
    - 변경: memberId:RefreshToken(String)
    - 변경 이유: 탈취된 RT의 재사용 시도 발견 시 RT를 삭제할 수 있으려면 key가 memberId여야 함

--- src/main/java/podo/odeego/domain/refreshtoken/repository/config/RedisConfig.java
+++ src/main/java/podo/odeego/domain/refreshtoken/repository/config/RedisConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -28,12 +27,10 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-		RedisTemplate<String, Long> redisTemplate = new RedisTemplate<>();
+	public RedisTemplate<Long, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<Long, String> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(redisConnectionFactory);
 
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
 		return redisTemplate;
 	}
 }
