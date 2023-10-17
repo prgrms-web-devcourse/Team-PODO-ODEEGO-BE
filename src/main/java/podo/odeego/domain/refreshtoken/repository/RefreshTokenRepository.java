1601abfc7a53bf2eba7286e4a7eefb669ae219cb
hwankim123 <hwankim1941@gmail.com>
2023-10-17 13:41:06 +0900

    Refactor: save()메서드 수정
    - 수정된 RefreshToken 클래스의 객체를 받아 저장하도록 수정

--- src/main/java/podo/odeego/domain/refreshtoken/repository/RefreshTokenRepository.java
+++ src/main/java/podo/odeego/domain/refreshtoken/repository/RefreshTokenRepository.java
@@ -9,69 +9,82 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import podo.odeego.domain.refreshtoken.entity.LegacyDtoRefreshToken;
 import podo.odeego.domain.refreshtoken.entity.RefreshToken;
 
 @Repository
 public class RefreshTokenRepository {
 
-	private final RedisTemplate<String, Long> template;
+	private final RedisTemplate<Long, String> template;
 	private final long refreshTokenExpirationMillis;
 	private final ObjectMapper objectMapper;
 
 	public RefreshTokenRepository(
 		@Value("${refresh-token.expiration}") long refreshTokenExpirationMillis,
-		RedisTemplate<String, Long> template,
+		RedisTemplate<Long, String> template,
 		ObjectMapper objectMapper) {
 		this.refreshTokenExpirationMillis = refreshTokenExpirationMillis;
 		this.template = template;
 		this.objectMapper = objectMapper;
 	}
 
-	public void save(RefreshToken refreshToken, Long memberId) {
-		String serializedRefreshToken;
-		try {
-			serializedRefreshToken = objectMapper.writeValueAsString(refreshToken);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-
-		ValueOperations<String, Long> valueOperations = template.opsForValue();
-		valueOperations.set(serializedRefreshToken, memberId);
-		template.expire(serializedRefreshToken, refreshTokenExpirationMillis, TimeUnit.SECONDS);
+	public void save(RefreshToken refreshToken) {
+		ValueOperations<Long, String> valueOperations = template.opsForValue();
+		valueOperations.set(refreshToken.memberId(), refreshToken.token());
+		template.expire(refreshToken.memberId(), refreshTokenExpirationMillis, TimeUnit.SECONDS);
 	}
+	// public void save(RefreshToken refreshToken, Long memberId) {
+	// 	String serializedRefreshToken;
+	// 	try {
+	// 		serializedRefreshToken = objectMapper.writeValueAsString(refreshToken);
+	// 	} catch (JsonProcessingException e) {
+	// 		throw new RuntimeException(e);
+	// 	}
+	//
+	// 	ValueOperations<String, Long> valueOperations = template.opsForValue();
+	// 	valueOperations.set(serializedRefreshToken, memberId);
+	// 	template.expire(serializedRefreshToken, refreshTokenExpirationMillis, TimeUnit.SECONDS);
+	// }
 
-	public Optional<Long> findMemberIdByRefreshToken(RefreshToken refreshToken) {
-		ValueOperations<String, Long> valueOperations = template.opsForValue();
-		Long memberId;
-		try {
-			memberId = valueOperations.get(objectMapper.writeValueAsString(refreshToken));
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
+	public Optional<RefreshToken> findByMemberId(Long memberId) {
+		ValueOperations<Long, String> valueOperations = template.opsForValue();
+		String refreshToken = valueOperations.get(memberId);
 
-		if (Objects.isNull(memberId)) {
+		if (Objects.isNull(refreshToken)) {
 			return Optional.empty();
 		}
-		return Optional.of(memberId);
+		return Optional.of(new RefreshToken(memberId, refreshToken));
 	}
 
-	public void updateRefreshToken(RefreshToken oldrefreshToken, RefreshToken newRefreshToken) {
-		String serializedOldRefreshToken, serializedNewRefreshToken;
-		try {
-			serializedOldRefreshToken = objectMapper.writeValueAsString(oldrefreshToken);
-			serializedNewRefreshToken = objectMapper.writeValueAsString(newRefreshToken);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-		template.rename(serializedOldRefreshToken, serializedNewRefreshToken);
-	}
+	// public Optional<Long> findMemberIdByRefreshToken(RefreshToken refreshToken) {
+	// 	ValueOperations<String, Long> valueOperations = template.opsForValue();
+	// 	Long memberId;
+	// 	try {
+	// 		memberId = valueOperations.get(objectMapper.writeValueAsString(refreshToken));
+	// 	} catch (JsonProcessingException e) {
+	// 		throw new RuntimeException(e);
+	// 	}
+	//
+	// 	if (Objects.isNull(memberId)) {
+	// 		return Optional.empty();
+	// 	}
+	// 	return Optional.of(memberId);
+	// }
 
-	//TODO: 컴파일 에러 방지
-	public Optional<LegacyDtoRefreshToken> findById() {
+	// public void updateRefreshToken(RefreshToken oldrefreshToken, RefreshToken newRefreshToken) {
+	// 	String serializedOldRefreshToken, serializedNewRefreshToken;
+	// 	try {
+	// 		serializedOldRefreshToken = objectMapper.writeValueAsString(oldrefreshToken);
+	// 		serializedNewRefreshToken = objectMapper.writeValueAsString(newRefreshToken);
+	// 	} catch (JsonProcessingException e) {
+	// 		throw new RuntimeException(e);
+	// 	}
+	// 	template.rename(serializedOldRefreshToken, serializedNewRefreshToken);
+	// }
+
+	//TODO(10.16): 컴파일 에러 방지
+	public Optional<RefreshToken> findById() {
 		return Optional.empty();
 	}
 
--- src/test/java/podo/odeego/domain/refreshtoken/repository/RefreshTokenRepositoryTest.java
+++ src/test/java/podo/odeego/domain/refreshtoken/repository/RefreshTokenRepositoryTest.java
@@ -23,85 +23,87 @@ class RefreshTokenRepositoryTest {
 	@DisplayName("RefreshToken을 저장할 수 있습니다.")
 	public void saveRefreshToken() {
 		//given
-		RefreshToken refreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
 		Long memberId = 1L;
+		RefreshToken refreshToken = new RefreshToken(memberId, UUID.randomUUID().toString());
 
 		//when
-		refreshTokenRepository.save(refreshToken, memberId);
-		Long actualMemberId = refreshTokenRepository.findMemberIdByRefreshToken(refreshToken).get();
+		refreshTokenRepository.save(refreshToken);
 
 		//then
-		assertThat(actualMemberId).isEqualTo(1L);
+		Optional<RefreshToken> expectedRefreshToken = refreshTokenRepository.findByMemberId(memberId);
+		assertThat(expectedRefreshToken).isPresent().get()
+			.extracting("memberId", "token")
+			.containsExactly(refreshToken.memberId(), refreshToken.token());
 	}
 
-	@Test
-	@DisplayName("RefreshToken 객체의 필드값과 일치하는 데이터가 존재한다면 memberId를 조회할 수 있습니다.")
-	void findMemberIdByRefreshToken() {
-		//given
-		RefreshToken refreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
-		Long memberId = 1L;
-		refreshTokenRepository.save(refreshToken, memberId);
+	// @Test
+	// @DisplayName("RefreshToken 객체의 필드값과 일치하는 데이터가 존재한다면 memberId를 조회할 수 있습니다.")
+	// void findMemberIdByRefreshToken() {
+	// 	//given
+	// 	RefreshToken refreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
+	// 	Long memberId = 1L;
+	// 	refreshTokenRepository.save(refreshToken, memberId);
+	//
+	// 	//when
+	// 	Optional<Long> actualMemberId = refreshTokenRepository.findMemberIdByRefreshToken(refreshToken);
+	//
+	// 	//then
+	// 	assertThat(actualMemberId).isPresent().get()
+	// 		.isEqualTo(1L);
+	// }
+	//
+	// @Test
+	// @DisplayName("RefreshToken 객체의 isValid 필드는 일치하지만 token 필드가 일치하지 않으면 memberId를 조회할 수 없습니다.")
+	// void findMemberIdByRefreshTokenFailByToken() {
+	// 	//given
+	// 	RefreshToken refreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
+	// 	Long memberId = 1L;
+	// 	refreshTokenRepository.save(refreshToken, memberId);
+	//
+	// 	RefreshToken notExistRefreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
+	//
+	// 	//when
+	// 	Optional<Long> actualMemberId = refreshTokenRepository.findMemberIdByRefreshToken(notExistRefreshToken);
+	//
+	// 	//then
+	// 	assertThat(actualMemberId).isEmpty();
+	// }
 
-		//when
-		Optional<Long> actualMemberId = refreshTokenRepository.findMemberIdByRefreshToken(refreshToken);
-
-		//then
-		assertThat(actualMemberId).isPresent().get()
-			.isEqualTo(1L);
-	}
-
-	@Test
-	@DisplayName("RefreshToken 객체의 isValid 필드는 일치하지만 token 필드가 일치하지 않으면 memberId를 조회할 수 없습니다.")
-	void findMemberIdByRefreshTokenFailByToken() {
-		//given
-		RefreshToken refreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
-		Long memberId = 1L;
-		refreshTokenRepository.save(refreshToken, memberId);
-
-		RefreshToken notExistRefreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
-
-		//when
-		Optional<Long> actualMemberId = refreshTokenRepository.findMemberIdByRefreshToken(notExistRefreshToken);
-
-		//then
-		assertThat(actualMemberId).isEmpty();
-	}
-
-	@Test
-	@DisplayName("RefreshToken 객체의 token 필드는 일치하지만 isValid 필드가 일치하지 않으면 memberId를 조회할 수 없습니다.")
-	void findMemberIdByRefreshTokenFailByIsValid() {
-		//given
-		RefreshToken refreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
-		Long memberId = 1L;
-		refreshTokenRepository.save(refreshToken, memberId);
-
-		RefreshToken notExistRefreshToken = new RefreshToken(refreshToken.getToken(), false);
-
-		//when
-		Optional<Long> actualMemberId = refreshTokenRepository.findMemberIdByRefreshToken(notExistRefreshToken);
-
-		//then
-		assertThat(actualMemberId).isEmpty();
-	}
-
-	@Test
-	@DisplayName("RefreshToken을 새로운 RefreshToken으로 갱신할 수 있습니다.")
-	void updateRefreshToken() {
-		//given
-		RefreshToken oldRefreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
-		refreshTokenRepository.save(oldRefreshToken, 1L);
-
-		RefreshToken newRefreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
-
-		//when
-		refreshTokenRepository.updateRefreshToken(oldRefreshToken, newRefreshToken);
-
-		//then
-		Optional<Long> notExistsMemberId = refreshTokenRepository.findMemberIdByRefreshToken(oldRefreshToken);
-		assertThat(notExistsMemberId).isEmpty();
-
-		Optional<Long> actualMemberId = refreshTokenRepository.findMemberIdByRefreshToken(newRefreshToken);
-		assertThat(actualMemberId).isPresent().get()
-			.isEqualTo(1L);
-	}
+	// @Test
+	// @DisplayName("RefreshToken 객체의 token 필드는 일치하지만 isValid 필드가 일치하지 않으면 memberId를 조회할 수 없습니다.")
+	// void findMemberIdByRefreshTokenFailByIsValid() {
+	// 	//given
+	// 	RefreshToken refreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
+	// 	Long memberId = 1L;
+	// 	refreshTokenRepository.save(refreshToken, memberId);
+	//
+	// 	RefreshToken notExistRefreshToken = new RefreshToken(refreshToken.getToken(), false);
+	//
+	// 	//when
+	// 	Optional<Long> actualMemberId = refreshTokenRepository.findMemberIdByRefreshToken(notExistRefreshToken);
+	//
+	// 	//then
+	// 	assertThat(actualMemberId).isEmpty();
+	// }
+	//
+	// @Test
+	// @DisplayName("RefreshToken을 새로운 RefreshToken으로 갱신할 수 있습니다.")
+	// void updateRefreshToken() {
+	// 	//given
+	// 	RefreshToken oldRefreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
+	// 	refreshTokenRepository.save(oldRefreshToken, 1L);
+	//
+	// 	RefreshToken newRefreshToken = new RefreshToken(UUID.randomUUID().toString(), true);
+	//
+	// 	//when
+	// 	refreshTokenRepository.updateRefreshToken(oldRefreshToken, newRefreshToken);
+	//
+	// 	//then
+	// 	Optional<Long> notExistsMemberId = refreshTokenRepository.findMemberIdByRefreshToken(oldRefreshToken);
+	// 	assertThat(notExistsMemberId).isEmpty();
+	//
+	// 	Optional<Long> actualMemberId = refreshTokenRepository.findMemberIdByRefreshToken(newRefreshToken);
+	// 	assertThat(actualMemberId).isPresent().get()
+	// 		.isEqualTo(1L);
+	// }
 }
