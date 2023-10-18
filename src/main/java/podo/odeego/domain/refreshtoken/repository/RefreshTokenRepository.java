7d1133469f5b56f817e1dbdeda8355b08f73b94a
hwankim123 <hwankim1941@gmail.com>
2023-10-18 15:15:25 +0900

    Fix: Refresh Token의 만료시간이 Milli Seconds 단위로 저장되도록 수정

--- src/main/java/podo/odeego/domain/refreshtoken/repository/RefreshTokenRepository.java
+++ src/main/java/podo/odeego/domain/refreshtoken/repository/RefreshTokenRepository.java
@@ -16,28 +16,24 @@ public class RefreshTokenRepository {
 
 	private final RedisTemplate<Long, String> redisTemplate;
 	private final long refreshTokenExpirationMillis;
-	// private final ObjectMapper objectMapper;
 
 	public RefreshTokenRepository(
 		@Value("${refresh-token.expiration}") long refreshTokenExpirationMillis,
 		RedisTemplate<Long, String> redisTemplate) {
-		// RedisTemplate<Long, String> template,
-		// ObjectMapper objectMapper) {
 		this.refreshTokenExpirationMillis = refreshTokenExpirationMillis;
 		this.redisTemplate = redisTemplate;
-		// this.objectMapper = objectMapper;
 	}
 
 	public void save(RefreshToken refreshToken) {
 		ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
 		valueOperations.set(refreshToken.memberId(), refreshToken.token());
-		redisTemplate.expire(refreshToken.memberId(), refreshTokenExpirationMillis, TimeUnit.SECONDS);
+		redisTemplate.expire(refreshToken.memberId(), refreshTokenExpirationMillis, TimeUnit.MILLISECONDS);
 	}
 
 	public void update(RefreshToken refreshToken) {
 		ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
 		valueOperations.setIfPresent(refreshToken.memberId(), refreshToken.token(),
-			refreshTokenExpirationMillis, TimeUnit.SECONDS);
+			refreshTokenExpirationMillis, TimeUnit.MILLISECONDS);
 	}
 
 	public void deleteByMemberId(Long memberId) {
@@ -52,21 +48,5 @@ public class RefreshTokenRepository {
 			return Optional.empty();
 		}
 		return Optional.of(new RefreshToken(memberId, refreshToken));
-	}
-
-	// public void updateRefreshToken(RefreshToken oldrefreshToken, RefreshToken newRefreshToken) {
-	// 	String serializedOldRefreshToken, serializedNewRefreshToken;
-	// 	try {
-	// 		serializedOldRefreshToken = objectMapper.writeValueAsString(oldrefreshToken);
-	// 		serializedNewRefreshToken = objectMapper.writeValueAsString(newRefreshToken);
-	// 	} catch (JsonProcessingException e) {
-	// 		throw new RuntimeException(e);
-	// 	}
-	// 	template.rename(serializedOldRefreshToken, serializedNewRefreshToken);
-	// }
-
-	//TODO(10.16): 컴파일 에러 방지
-	public Optional<RefreshToken> findById() {
-		return Optional.empty();
 	}
 }
